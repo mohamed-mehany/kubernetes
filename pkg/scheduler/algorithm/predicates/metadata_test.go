33eb631751e85b6d334a4bba9c4506112da61640
Ahmad Diaa <ahmaddiaa93@gmail.com>
2018-07-30 06:26:07 +0200

    add topologyValue map to shallowCopy test

--- pkg/scheduler/algorithm/predicates/metadata_test.go
+++ pkg/scheduler/algorithm/predicates/metadata_test.go
@@ -475,6 +475,10 @@ func TestPredicateMetadata_ShallowCopy(t *testing.T) {
 				},
 			},
 		},
+		topologyValueToAntiAffinityPods: map[string][]string{
+			"machine1": []string{"p1", "p2"},
+			"machine2": []string{"p3"},
+		},
 		nodeNameToMatchingAffinityPods: map[string][]*v1.Pod{
 			"nodeA": {
 				&v1.Pod{ObjectMeta: metav1.ObjectMeta{Name: "p1", Labels: selector1},
